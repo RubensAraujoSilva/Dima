31ba4efc2a44b2742c7ade80eafcf6916feff90b
Rubens Araújo <rubens@octosoft.com.br>
2025-12-26 20:45:29 -0300

    Iniciando projeto web blazor webassembly com mudblazor

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,25 +1,86 @@
-using Dima.Api;
-using Dima.Api.Common.Api;
+using System.Security.Claims;
 using Dima.Api.Common.Endpoints;
+using Dima.Api.Data;
+using Dima.Api.Handlers;
+using Dima.Api.Models;
+using Dima.Core.Handlers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddConfiguration();
-builder.AddSecurity();
-builder.AddDataContexts();
-builder.AddCrosOrigin();
-builder.AddSwagger();
-builder.AddServices();
+
+var cnnString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+
+builder.Services.AddDbContext<AppDbContext>(x => { x.UseSqlServer(cnnString); });
+
+builder.Services.AddIdentityCore<User>()
+    .AddRoles<IdentityRole<long>>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddApiEndpoints();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x => { x.CustomSchemaIds(n => n.FullName); });
+
+//Adicione Autentica��o e Autoriza��o
+builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
+    .AddIdentityCookies();
+
+builder.Services.AddAuthorization();
+
+//Servi�os  DI
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
 
 var app = builder.Build();
 
-if(app.Environment.IsDevelopment())
-    app.ConfigureDevEnvironment();
+//Use a Autentica��o e Autoriza��o
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.UseCors(ApiConfiguration.CorsPolicyName);
-app.UseSecurity();
+//Interface do Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
+//Rotas da API 
+app.MapGet("/", () => new { message = "OK" });
+
 app.MapEndpoints();
+
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapIdentityApi<User>();
+
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapPost("/logout", async (
+        SignInManager<User> signInManager) =>
+    {
+        await signInManager.SignOutAsync();
+        return Results.Ok();
+    }).RequireAuthorization();
+
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapGet("/roles", (ClaimsPrincipal user) =>
+    {
+        if (user.Identity is null || !user.Identity.IsAuthenticated)
+            return Results.Unauthorized();
+
+        var idendity = (ClaimsIdentity)user.Identity;
+        var roles = idendity
+            .FindAll(idendity.RoleClaimType)
+            .Select(c => new
+            {
+                c.Issuer,
+                c.OriginalIssuer, 
+                c.Type, 
+                c.Value, 
+                c.ValueType
+            });
+
+        return TypedResults.Json(roles);
+    }).RequireAuthorization();
 
 app.Run();
